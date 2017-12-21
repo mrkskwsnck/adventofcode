3764120bf30004a1554fdd5ef54e6935b52a7612
Markus Kwaśnicki <markus.kwasnicki@hs-mainz.de>
2017-12-21 18:06:22 +0100

    Solution for day 5 part 1 and 2

---
+++ 2017/day/5/solution_part2.lua
@@ -0,0 +1,54 @@
+#!/usr/bin/env lua
+
+------------------------------------------------------
+--- Day 5: A Maze of Twisty Trampolines, All Alike ---
+------------------------------------------------------
+
+local ram = {}      -- The instruction set
+local pointer = 1   -- Position to jump from
+                    -- to the next instruction
+local steps = 0     -- Step counter
+
+function printram ()
+    if arg[1] ~= "DEBUG" then
+        return
+    end
+
+    for i = 1, #ram do
+        if i == pointer then
+            io.write(string.format("(%i)", ram[i]))
+        else
+            io.write(ram[i])
+        end
+        io.write(" ")
+    end
+    io.write("\b\n")
+end
+
+function jump ()
+    local offset = ram[pointer]
+
+    if offset >= 3 then
+        ram[pointer] = ram[pointer] - 1
+    else
+        ram[pointer] = ram[pointer] + 1
+    end
+    
+    pointer = pointer + offset
+    steps = steps + 1
+end
+
+-- Built up instruction set
+for line in io.lines() do
+    local n = tonumber(line)
+    table.insert(ram, n)
+end
+
+printram()  -- Initial state
+
+while pointer >= 1 and pointer <= #ram do
+    jump()
+    printram()
+end
+
+io.write(steps, "\n")   -- Steps taken to reach the exit
