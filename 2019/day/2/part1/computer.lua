639c4c7132e63de134f68fb60f567ab612b5b1ca
Markus Kwaśnicki <486983+mrkskwsnck@users.noreply.github.com>
2019-12-04 14:42:58 +0100

    Solution for puzzle 2019, day 2, part 1

--- 2019/day/2/part1/computer.lua
+++ 2019/day/2/part1/computer.lua
@@ -1,1 +1,36 @@
--- One program per line
+#!/usr/bin/env lua5.3
+
+local program = require "program"
+
+function main ()
+  local file = io.open("program.txt", "r")
+  local progstr = file:read("l")
+  file:close()
+
+  local p = program:new(progstr)
+  p:restoregravityassist()
+  io.write(string.format("Input: %s\n", p:tostring()))
+  local opcode = p:nextopcode()
+  
+  while opcode ~= 99 do
+    if opcode == 1 then
+      local operand1 = p:nextval()
+      local operand2 = p:nextval()
+      local sum = operand1 + operand2
+      local pos = p:nextstore()
+      p:store(pos, sum)
+    elseif opcode == 2 then
+      local operand1 = p:nextval()
+      local operand2 = p:nextval()
+      local prod = operand1 * operand2
+      local pos = p:nextstore()
+      p:store(pos, prod)
+    end
+    opcode = p:nextopcode()
+  end
+  
+  io.write(string.format("Output: %s\n", p:tostring()))
+  io.write("\n")
+end
+
+main()
---
+++ 2019/day/2/part1/program.lua
@@ -0,0 +1,63 @@
+local M = {}
+
+local function tointcode(progstr)
+  local intcode = {}
+  for int in progstr:gmatch("%d+") do
+    table.insert(intcode, tonumber(int))
+  end
+  return intcode
+end
+
+function M:new (progstr)
+  local t = {
+    intcode = tointcode(progstr),
+    pos = 1
+  }
+  self.__index = self
+  setmetatable(t, self)
+  return t
+end
+
+function M:__stepforward ()
+  self.pos = self.pos + 1
+  io.write(string.format("Set new position to %d\n", self.pos - 1))
+end
+
+function M:nextopcode ()
+  local opcode = self.intcode[self.pos]
+  io.write(string.format("Read opcode %d at position %d\n", opcode, self.pos - 1))
+  self:__stepforward()
+  return opcode
+end
+
+function M:nextval ()
+  local pos = self.intcode[self.pos] + 1
+  local val = self.intcode[pos]
+  io.write(string.format("Read value %d at position %d\n", val, pos - 1))
+  self:__stepforward()
+  return val
+end
+
+function M:nextstore ()
+  local pos = self.intcode[self.pos] + 1
+  io.write(string.format("Read store position %d\n", pos - 1))
+  self:__stepforward()
+  return pos
+end
+
+function M:store (pos, val)
+  io.write(string.format("Store %d at position %d\n", val, pos - 1))
+  self.intcode[pos] = val
+end
+
+function M:tostring ()
+  return table.concat(self.intcode, ",")
+end
+
+function M:restoregravityassist ()
+  -- Restore the gravity assist program to the "1202 program alarm" state
+  self.intcode[1 + 1] = 12  -- Replace position 1 with the value 12
+  self.intcode[2 + 1] = 2   -- Replace position 2 with the value 2
+end
+
+return M
